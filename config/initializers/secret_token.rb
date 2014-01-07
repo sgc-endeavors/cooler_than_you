c94d647922001d71d39232110cc8d902b377d23c
Gerard Cote <grcote@gmail.com>
2014-01-07 15:30:56 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoolerThanYou::Application.config.secret_token = 'd341d5473b200f6adb26f9392bfddd708fc3131af7c9272dd03a947fe667ecf7794616c65a009d2999b92b76b391d476868f170575e07bd8ee6cbda42c540ea4'
